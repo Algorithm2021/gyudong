cd6c1c107d366eefa048dfec6ab57adecd2aa53d
gyudong <kelee0812.2@gmail.com>
2021-01-02 15:09:26 +0900

    백준 S2 1012번 유기농 배추

--- 1012.py
+++ 1012.py
@@ -1,19 +1,22 @@
-from itertools import product
 import sys
 
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**5)
 
 
 def dfs(xp, yp):
-    new_p = [[int(xp-1), int(xp+1)], [int(yp-1), int(yp+1)]]
-    comb = list(product(*new_p))
+    field[yp][xp] = 0
 
-    for point in comb:
-        new_x, new_y = point
+    if xp-1>=0 and field[yp][xp-1]!=0:
+        dfs(xp-1, yp)
 
-        if field[new_y][new_x]==1 and -1 < new_x < M and -1 < new_y < M:
-            visited[new_y][new_x] = True
-            dfs(new_x, new_y)
+    if xp+1<M and field[yp][xp+1]!=0:
+        dfs(xp+1, yp)
+
+    if yp-1>=0 and field[yp-1][xp]!=0:
+        dfs(xp, yp-1)
+
+    if yp+1<N and field[yp+1][xp]!=0:
+        dfs(xp, yp+1)
 
 
 T = int(input())
@@ -23,15 +26,17 @@ for i in range(T):
     worm = 0
 
     field = [[0 for m in range(M)] for n in range(N)]
-    visited = [[False for m in range(M)] for n in range(N)]
 
     for j in range(K):
         x, y = map(int, input().split())
         field[y][x] = 1
 
-    for m, n in product(range(M), range(N)):
-        if field[n][m]==1 and not visited[n][m]:
+    for p in range(M*N):
+        y = p//M
+        x = p - y*M
+
+        if field[y][x]==1:
             worm += 1
-            dfs(m, n)
+            dfs(x, y)
 
     print(worm)
